fa23d10836c82d0afe867d9e13036cd1cdac3256
Franco Selvarolo <fselvarolo28@gmail.com>
2024-04-04 00:11:30 -0300

    acomode segunda carga

--- src/Components/Comander/AdminPanel.jsx
+++ src/Components/Comander/AdminPanel.jsx
@@ -4,24 +4,31 @@ import Dashboard from "./DashBoard/DashBoard";
 import ComandasComponent from "./General/Comander";
 import ModalGen from "../Modal/ModalConfirmacion/Modal";
 import { Editer } from "../Categorias/Editer";
+import { useSelector } from "react-redux";
+import LoginComponent from "./LogIn/LogIn";
 
 
 export const AdminPanel = () => {
   const [panel, setPanel] = useState("General");
+  const { usuarioComander } = useSelector((state) => state.alldata);
   
   return (
-    <div>
-      <div style={{padding:"1rem",display:"flex", justifyContent:"center" ,gap:"1rem"}}>
-      <button className="buttonDash" onClick={() => setPanel("General")}>General</button>
-      <button className="buttonDash" onClick={() => setPanel("Estadisticas")}>Estadisticas</button>
-      <button className="buttonDash" onClick={() => setPanel("Otros")}>Otros</button>
-      <ModalGen Child={<Editer />} txtBtn="Editar Producto"  />
-      </div>
-      
-      {/* Aquí puedes renderizar el panel según el estado "panel" */}
-      {panel === "General" && <ComandasComponent />}
-      {panel === "Estadisticas" && <Dashboard />}
-      {panel === "Otros" && <Dashboard />}
-    </div>
+    <>
+      {usuarioComander?
+        <div>
+          <div style={{padding:"1rem",display:"flex", justifyContent:"center" ,gap:"1rem"}}>
+          <button className="buttonDash" onClick={() => setPanel("General")}>General</button>
+          <button className="buttonDash" onClick={() => setPanel("Estadisticas")}>Estadisticas</button>
+          <button className="buttonDash" onClick={() => setPanel("Otros")}>Otros</button>
+          <ModalGen Child={<Editer />} txtBtn="Editar Producto"  />
+          </div>
+          
+          {/* Aquí puedes renderizar el panel según el estado "panel" */}
+          {panel === "General" && <ComandasComponent />}
+          {panel === "Estadisticas" && <Dashboard />}
+          {panel === "Otros" && <Dashboard />}
+        </div>:<LoginComponent />
+      }
+    </>
   );
 };
